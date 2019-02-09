21048645d025e59144d82344070d3d8d323c9553
Thomas Fjørstad <thomas.fjorstad@bouvet.no>
2019-02-09 21:54:48 +0100

    Added Service and a Adapter

---
+++ app/src/main/java/no/tfjorstad/trainingapp/activity/MainActivity.java
@@ -0,0 +1,55 @@
+package no.tfjorstad.trainingapp.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import no.tfjorstad.trainingapp.R;
+import no.tfjorstad.trainingapp.adapter.TrainingAdapter;
+import no.tfjorstad.trainingapp.service.TrainingService;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TrainingService trainingService;
+    private TrainingAdapter trainingAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        trainingService = new TrainingService();
+        trainingAdapter = new TrainingAdapter(trainingService.getTrainings(), getApplicationContext());
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview_main_trainings);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.setAdapter(trainingAdapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.new_traingsrec:
+                newTraining();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    private void newTraining() {
+        Intent intent = new Intent(getApplicationContext(), NewTrainingRecord.class);
+        startActivity(intent);
+    }
+}
+
---
+++ app/src/main/java/no/tfjorstad/trainingapp/activity/NewTrainingRecord.java
@@ -0,0 +1,33 @@
+package no.tfjorstad.trainingapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import no.tfjorstad.trainingapp.R;
+
+public class NewTrainingRecord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_training_record);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.new_training_session_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            default:
+
+        return super.onOptionsItemSelected(item);
+        }
+    }
+}
